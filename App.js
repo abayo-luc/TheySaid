253f50eefa0c95db20376c50d79bc60d701ee36e
Luc <luc.bayo@gmail.com>
2019-05-29 12:10:33 +0200

    fixes hound complains

--- App.js
+++ App.js
@@ -10,10 +10,8 @@ const styles = StyleSheet.create({
   }
 });
 
-export default () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-};
+export default () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+  </View>
+);
