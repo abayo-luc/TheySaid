6b5004ab7ec63e919cc0226f8dfe1afb83c093c6
Luc <luc.bayo@gmail.com>
2019-05-29 18:00:21 +0200

    ajdust test for profile and home

--- __test__/screens/profile.spec.js
+++ __test__/screens/profile.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Profile from "../../src/screens/Profile";
+import Profile from "../../src/screens/Profile/Profile";
 
 describe("Profile Screen", () => {
   test("renders correctly", () => {
