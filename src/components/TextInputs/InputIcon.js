48d91f65fa662bf818dbdd3c7a96b2fe86f0923c
Luc <luc.bayo@gmail.com>
2019-05-29 17:58:15 +0200

    [feature #166258383] build home page - Add header component with search input - Add ListIem component - Add scrollable flatlist - Add list header

--- src/components/TextInputs/InputIcon.js
+++ src/components/TextInputs/InputIcon.js
@@ -1,9 +1,13 @@
 import React from "react";
+<<<<<<< HEAD
 import PropTypes from "prop-types";
+=======
+>>>>>>> [feature #166258383] build home page
 import { View, TextInput } from "react-native";
 import styles from "./styles";
 import CustomIcon from "../Icons/CustomIcons";
 
+<<<<<<< HEAD
 const InputIcon = ({ onChangeText, value }) => (
   <View style={styles.container}>
     <CustomIcon name="search" size={32} color={styles.$themeColor} />
@@ -27,4 +31,19 @@ InputIcon.defaultProps = {
   onChangeText: () => null,
   value: ""
 };
+=======
+const InputIcon = () => {
+  return (
+    <View style={styles.container}>
+      <CustomIcon name="search" size={32} color={styles.$themeColor} />
+      <TextInput
+        style={styles.input}
+        placeholder="Search..."
+        placeholderTextColor={styles.$themeColor}
+      />
+    </View>
+  );
+};
+
+>>>>>>> [feature #166258383] build home page
 export default InputIcon;
