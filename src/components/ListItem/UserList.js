86e4060d0b6dd6283b6d70607a6f17f035cd3b04
Luc <luc.bayo@gmail.com>
2019-05-30 18:54:07 +0200

    [Feature #166258383] integrate end-point -Integrate redux -Fetches data -Integrate redux-sage -Display data -Optimize flatlist

--- src/components/ListItem/UserList.js
+++ src/components/ListItem/UserList.js
@@ -53,4 +53,14 @@ UserList.defaultProps = {
   email: ""
 };
 
+UserList.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  email: PropTypes.string
+};
+
+UserList.defaultProps = {
+  email: ""
+};
+
 export default UserList;
