d8194cd2d2fc530abce55997237860b53b0d8653
Luc <luc.bayo@gmail.com>
2019-05-31 12:38:49 +0200

    Set fake api_key for test env

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -2,10 +2,12 @@ import { API_KEY } from "react-native-dotenv";
 
 const GITHUB_API = `https://api.github.com/search/users?q=location:lagos+language:java`;
 
+const apiKey = process.env.NODE_ENV === "test" ? "api_key" : API_KEY;
+
 export const getUsers = page => {
   const headers = {
     "Content-Type": "application/json",
-    Authorization: `token ${API_KEY}`
+    Authorization: `token ${apiKey}`
   };
 
   return fetch(`${GITHUB_API}&per_page=50&page=${page}`, {
