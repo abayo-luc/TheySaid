252df36f07491e581f472092db247521a5957463
Luc <luc.bayo@gmail.com>
2019-05-31 13:02:46 +0200

    change the way of saving env variables

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,13 +1,15 @@
-import { API_KEY } from "react-native-dotenv";
+let API_KEY;
+if (process.env.NODE_ENV !== "test") {
+  // eslint-disable-next-line global-require
+  ({ API_KEY } = require("../config/keys.json"));
+}
 
 const GITHUB_API = `https://api.github.com/search/users?q=location:lagos+language:java`;
-
-const apiKey = process.env.NODE_ENV === "test" ? "api_key" : API_KEY;
-
+// const { API_KEY } = keys;
 export const getUsers = page => {
   const headers = {
     "Content-Type": "application/json",
-    Authorization: `token ${apiKey}`
+    Authorization: `token ${API_KEY}`
   };
 
   return fetch(`${GITHUB_API}&per_page=50&page=${page}`, {
